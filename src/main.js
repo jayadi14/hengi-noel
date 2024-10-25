02c296cfb3e5b8dcdec560bca4ddf7385ec8c1f7
Jayadi Akbar <87864995+jayadi14@users.noreply.github.com>
2024-10-25 22:07:50 +0700

    first c ommit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// Load the header HTML
+fetch("layout/header.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("header-container").innerHTML = data;
+
+    // Fetch navigation links from JSON
+    return fetch("navLinks.json");
+  })
+  .then((response) => response.json())
+  .then((navLinks) => {
+    const navContainer = document.getElementById("nav-links");
+
+    // Loop through each link and create an element
+    navLinks.forEach((link) => {
+      const anchor = document.createElement("a");
+      anchor.href = link.url;
+
+      const div = document.createElement("div");
+      div.className =
+        "p-2 w-[120px] hover:text-white hover:bg-sky-400 duration-300";
+      div.textContent = link.name;
+
+      anchor.appendChild(div);
+      navContainer.appendChild(anchor);
+    });
+  })
+  .catch((error) =>
+    console.error("Error loading header or navigation:", error)
+  );

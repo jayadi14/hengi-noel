c294eded430791ddd5a2d6bd2caf8842354f9b23
Jayadi Akbar <87864995+jayadi14@users.noreply.github.com>
2024-10-28 00:25:40 +0700

    home

--- src/main.js
+++ src/main.js
@@ -1,48 +1,48 @@
 // Load the layout HTML, which includes the header container
-fetch("layout/layout.html")
+fetch('layout/layout.html')
   .then((response) => {
-    console.log("Layout response status:", response.status); // Log the response status
+    console.log('Layout response status:', response.status); // Log the response status
     return response.text();
   })
   .then((data) => {
-    document.getElementById("main-content-container").innerHTML = data;
+    document.getElementById('main-content-container').innerHTML = data;
 
     // Now that layout is loaded, we can load the header
-    return fetch("layout/header.html");
+    return fetch('layout/header.html');
   })
   .then((response) => {
-    console.log("Header response status:", response.status); // Log the response status
+    console.log('Header response status:', response.status); // Log the response status
     return response.text();
   })
   .then((data) => {
-    document.getElementById("header-container").innerHTML = data;
+    document.getElementById('header-container').innerHTML = data;
 
     // Fetch navigation links from JSON
-    return fetch("assets/json/navLinks.json");
+    return fetch('assets/json/navLinks.json');
   })
   .then((response) => {
-    console.log("Navigation JSON response status:", response.status); // Log the response status
+    console.log('Navigation JSON response status:', response.status); // Log the response status
     return response.json();
   })
   .then((navLinks) => {
-    const navContainer = document.getElementById("nav-links");
+    const navContainer = document.getElementById('nav-links');
 
     // Loop through each link and create an element
     navLinks.forEach((link) => {
-      const anchor = document.createElement("a");
+      const anchor = document.createElement('a');
       anchor.href = link.url;
 
-      const div = document.createElement("div");
+      const div = document.createElement('div');
       div.className =
-        "p-2 w-[120px] hover:text-white hover:bg-sky-400 duration-300";
+        'p-2 w-[120px] text-white border-8 border-black hover:border-b-yellow-400 duration-300';
       div.textContent = link.name;
 
       anchor.appendChild(div);
       navContainer.appendChild(anchor);
     });
 
-    console.log("Navigation loaded successfully"); // Confirm successful loading
+    console.log('Navigation loaded successfully'); // Confirm successful loading
   })
   .catch((error) =>
-    console.error("Error loading header or navigation:", error)
+    console.error('Error loading header or navigation:', error),
   );
